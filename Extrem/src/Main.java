a1d050fc3a881c7fa5148c6ea02db4d26d2e0bac
Alann <DLRegine@10.121.61.42>
2016-02-05 11:06:27 +0100

    Remise du titre à 0

--- Extrem/src/Main.java
+++ Extrem/src/Main.java
@@ -21,7 +21,7 @@ public class Main extends Application
 			primaryStage.centerOnScreen();
 			primaryStage.setResizable(false);
 			primaryStage.setScene(scene);
-			primaryStage.setTitle("efef");
+			primaryStage.setTitle("Extrem JV");
 			primaryStage.show();
 		}
 		catch (IOException e)
